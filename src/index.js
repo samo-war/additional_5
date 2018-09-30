b2b7222ad0263b4784228d1c50d02e18762f2704
samo-war <samo_war@mail.ru>
2018-09-30 17:47:08 +0300

    Task 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBr = bracketsConfig.map(item => item[0]);
+  const closeBr = bracketsConfig.map(item => item[1]);
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (openBr.includes(str[i])) {
+      stack.push(str[i]);
+    }
+
+    if (closeBr.includes(str[i])) {
+      if (closeBr.indexOf(str[i]) !== openBr.indexOf(stack.pop())) {
+        return false;
+      }
+    }
+  }
+
+  return !stack.length;
 }
